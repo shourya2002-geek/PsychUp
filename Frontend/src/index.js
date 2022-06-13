4e02ec66ccdfdc9c135587cb7b4b8c3b974f14c0
shourya2002-geek <13.shourya@gmail.com>
2022-06-13 20:49:34 +0530

    Initialized frontend

---
+++ Frontend/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Editor from "./components/Editor/Editor";
+import { CookiesProvider } from "react-cookie";
+
+
+ReactDOM.render(
+  <CookiesProvider>
+
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </CookiesProvider>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
